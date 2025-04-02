059402c6702375550a7f3919768587d612097130
Githyunmin <hmkoh25@g.shingu.com>
2025-04-02 15:50:47 +0900

    [Feat] Prefab 생성 및 활용 && 스포너를 통해서 인스턴스 생성

---
+++ Assets/script/Ball.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Plane")
+        {
+            Debug.Log("땅과충돌");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("트리거 안에 들어옴");
+    }
+}
---
+++ Assets/script/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+
+
+    public bool isGrounded = true;
+
+    public int coinCount = 0;
+    public int totalCoins = 5;
+
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorzontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        rb.velocity = new Vector3(moveHorzontal * moveSpeed, rb.velocity.y, moveVertical * moveSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Plane")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            coinCount++;
+            Destroy(other.gameObject);
+            Debug.Log($"코인수집 : {coinCount}/ {totalCoins}");
+        }
+
+        if (other.gameObject.tag == "Door" && coinCount == totalCoins)
+        {
+            Debug.Log("Clear");
+        }
+    }
+}
